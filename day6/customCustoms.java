430da6c485ccfbd1f0970e809f629e7e573a81f5
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2020-12-06 10:56:22 +0530

    Added solution for day 6 problem 2

--- day6/customCustoms.java
+++ day6/customCustoms.java
@@ -21,8 +21,40 @@ public class customCustoms {
     br.close();
     return result;
   }
+  public int returnYesCount() throws IOException{
+    BufferedReader br = new BufferedReader(new FileReader("day6/group.list"));
+    int result = 0;
+    int groupCount = 0;
+    String line = "";
+    Map<Character, Integer> map = new HashMap<Character, Integer>();
+    while((line = br.readLine()) != null) {
+      if(line.length() != 0) {
+        groupCount += 1;
+        for(int i = 0; i < line.length(); i++) {
+          if(!map.containsKey(line.charAt(i))) {
+            map.put(line.charAt(i), 1);
+          }
+          else {
+            map.put(line.charAt(i), map.get(line.charAt(i)) + 1);
+          }
+        }
+      }
+      else {
+        for(char character: map.keySet()) {
+          if(map.get(character) == groupCount) {
+            result += 1;
+          }
+        }
+        groupCount = 0;
+        map = new HashMap<Character, Integer>();
+      }
+    }
+    br.close();
+    return result;
+  }
   public static void main(String[] args) throws IOException {
     customCustoms obj = new customCustoms();
     System.out.println("Puzzle 1 solution = " + obj.returnCount());
+    System.out.println("Puzzle 2 solution = " + obj.returnYesCount());
   }
 }
