aa29d6ce5a466a1e8afa3e551376b28ea95c754d
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2020-12-08 12:01:04 +0530

    Added solution for day 8 puzzle 2

--- day8/handheldHalting.java
+++ day8/handheldHalting.java
@@ -13,21 +13,16 @@ public class handheldHalting {
       instructionList.add(line);
     }
     br.close();
+
     String instruction = "";
-    System.out.println(instructionList);
     boolean[] visited = new boolean[instructionList.size()];
-    System.out.println(visited[0]);
     int index = 0;
     while(true) {
-      System.out.println(index);
-      System.out.println(visited[index]);
       if(visited[index] == true) {
-        System.out.println("hello");
         break;
       }
       visited[index] = true;
       instruction = instructionList.get(index);
-      System.out.println(instruction);
       String[] instructions = instruction.split(" ");
       if(instructions[0].equals("nop")) {
         index += 1;
@@ -52,7 +47,7 @@ public class handheldHalting {
     }
     return accumulator;
   }
-  public int getFixedAAccumulator() {
+  public int getFixedAccumulator() throws IOException {
     BufferedReader br = new BufferedReader(new FileReader("day8/instruction.list"));
     List<String> instructionList = new ArrayList<String>();
     int accumulator = 0;
@@ -61,47 +56,61 @@ public class handheldHalting {
       instructionList.add(line);
     }
     br.close();
-    String instruction = "";
-    System.out.println(instructionList);
-    boolean[] visited = new boolean[instructionList.size()];
-    System.out.println(visited[0]);
-    int index = 0;
-    while(true) {
-      System.out.println(index);
-      System.out.println(visited[index]);
-      if(visited[index] == true) {
-        System.out.println("hello");
+
+    for(int i = 0; i < instructionList.size(); i++) {
+      ArrayList<String> moddedInstructionList = new ArrayList<String>(instructionList);
+      String[] moddedInstruction = moddedInstructionList.get(i).split(" ");
+      if(moddedInstruction[0].equals("jmp")) {
+        moddedInstructionList.set(i, "nop " + moddedInstruction[1]);
+      }
+      else if(moddedInstruction[0].equals("nop")) {
+        moddedInstructionList.set(i, "jmp " + moddedInstruction[1]);
+      }
+      else {
+        continue;
+      }
+      String instruction = "";
+      boolean[] visited = new boolean[moddedInstructionList.size()];
+      int index = 0;
+      accumulator = 0;
+      while(index < moddedInstructionList.size()) {
+        if(visited[index] == true) {
+          break;
+        }
+        visited[index] = true;
+        instruction = moddedInstructionList.get(index);
+        String[] instructions = instruction.split(" ");
+        if(instructions[0].equals("nop")) {
+          index += 1;
+        }
+        else if(instructions[0].equals("acc")) {
+          if(instructions[1].charAt(0) == '+') {
+            accumulator += Integer.parseInt(instructions[1].substring(1));
+          }
+          else {
+            accumulator -= Integer.parseInt(instructions[1].substring(1));
+          }
+          index += 1;
+        }
+        else if(instructions[0].equals("jmp")){
+          if(instructions[1].charAt(0) == '+') {
+            index += Integer.parseInt(instructions[1].substring(1));
+          }
+          else {
+            index -= Integer.parseInt(instructions[1].substring(1));
+          }
+        }
+      }
+      if(index >= moddedInstructionList.size() - 1) {
         break;
       }
-      visited[index] = true;
-      instruction = instructionList.get(index);
-      System.out.println(instruction);
-      String[] instructions = instruction.split(" ");
-      if(instructions[0].equals("nop")) {
-        index += 1;
-      }
-      else if(instructions[0].equals("acc")) {
-        if(instructions[1].charAt(0) == '+') {
-          accumulator += Integer.parseInt(instructions[1].substring(1));
-        }
-        else {
-          accumulator -= Integer.parseInt(instructions[1].substring(1));
-        }
-        index += 1;
-      }
-      else if(instructions[0].equals("jmp")){
-        if(instructions[1].charAt(0) == '+') {
-          index += Integer.parseInt(instructions[1].substring(1));
-        }
-        else {
-          index -= Integer.parseInt(instructions[1].substring(1));
-        }
-      }
     }
+    
     return accumulator;
   }
   public static void main(String[] args) throws IOException{
     handheldHalting obj = new handheldHalting();
     System.out.println("Puzzle 1 solution = " + obj.getAccumulator());
+    System.out.println("Puzzle 2 solution = " + obj.getFixedAccumulator());
   }
 }
