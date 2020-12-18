96dd1166aa0c79cd60d0bef54536e87167285a76
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2020-12-18 15:52:39 +0530

    Test commit

--- day_18/OperationOrder.java
+++ day_18/OperationOrder.java
@@ -65,6 +65,57 @@ public class OperationOrder {
     return result;
   }
 
+  // long solveExpression(String expression) {
+  //   Stack<Long> operands = new Stack<Long>();
+  //   Stack<Character> operators = new Stack<Character>();
+  //   int index = expression.length() - 1;
+  //   String number = "";
+  //   boolean pushFlag = false;
+  //   do {
+  //     char token = expression.charAt(index);
+  //     if(token == ' ') {
+  //       if(pushFlag) {
+  //         operands.push(Long.parseLong(number));
+  //         pushFlag = false;
+  //         number = "";
+  //       }
+  //     }
+  //     else if(token == '(') {
+  //       if(pushFlag) {
+  //         operands.push(Long.parseLong(number));
+  //         pushFlag = false;
+  //         number = "";
+  //       }
+  //       char operator;
+  //       while((operator = operators.pop()) != ')') {
+  //         long a = operands.pop();
+  //         long b = operands.pop();
+  //         operands.push(applyOperation(a, b, operator));
+  //       }
+  //     }
+  //     else if(token == '+' || token == '*' || token == ')') {
+  //       operators.push(token);
+  //     }
+  //     else if(token >= '0' && token <= '9') {
+  //       number = token + number;
+  //       pushFlag = true;
+  //       if(index == 0) {
+  //         operands.push(Long.parseLong(number));
+  //       }
+  //     }
+  //     index --;
+  //   } while(index >= 0);
+  //   long result = 0;
+  //   while(!operators.empty()) {
+  //     long a = operands.pop();
+  //     long b = operands.pop();
+  //     char operator = operators.pop();
+  //     result = applyOperation(a, b, operator);
+  //     operands.push(result);
+  //   }
+  //   return result;
+  // }
+
   public long solve(boolean precedence) throws IOException {
     BufferedReader br = new BufferedReader(new FileReader("day_18/operations.list"));
     String line;
@@ -79,6 +130,6 @@ public class OperationOrder {
   public static void main(String[] args) throws IOException{
     OperationOrder obj = new OperationOrder();
     System.out.println("Puzzle 1 solution = " + obj.solve(false));
-    // System.out.println("Puzzle 2 solution = " + obj.solve(true));
+    System.out.println("Puzzle 2 solution = " + obj.solve(true));
   }
 }
