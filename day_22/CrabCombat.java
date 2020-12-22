b573cfaaeb4acc90d1d99acf42743916c83286e2
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2020-12-22 20:12:37 +0530

    Added solution for puzzle 2 day 22

--- day_22/CrabCombat.java
+++ day_22/CrabCombat.java
@@ -48,9 +48,57 @@ public class CrabCombat {
     return score;
   }
 
+  int recursiveCombat(Deque<Integer> deck1, Deque<Integer> deck2) {
+    Set<List<Integer>> playedGames = new HashSet<>();
+    while(!deck1.isEmpty() && !deck2.isEmpty()) {
+      if(!playedGames.add(new ArrayList<Integer>(deck1))){
+        return 1;
+      }
+      int card1 = deck1.pop();
+      int card2 = deck2.pop();
+      if(deck1.size() >= card1 && deck2.size() >= card2) {
+        Deque<Integer> subDeck1 = new ArrayDeque<Integer>(new ArrayList<Integer>(deck1).subList(0, card1));
+        Deque<Integer> subDeck2 = new ArrayDeque<Integer>(new ArrayList<Integer>(deck2).subList(0, card2));
+        int winner = recursiveCombat(subDeck1, subDeck2);
+        if(winner == 1) {
+          deck1.addLast(card1);
+          deck1.addLast(card2);
+        }
+        else {
+          deck2.addLast(card2);
+          deck2.addLast(card1);
+        }
+      }
+      else {
+        if(card1 > card2) {
+          deck1.addLast(card1);
+          deck1.addLast(card2);
+        }
+        else {
+          deck2.addLast(card2);
+          deck2.addLast(card1);
+        }
+      }
+    }
+    return (deck1.isEmpty()) ? 2 : 1;
+  }
+
+  int getRecursiveCombatScore() {
+    int score = 0;
+    int winner = recursiveCombat(decks.get(0), decks.get(1)) - 1;
+    int i = 1;
+    while(!decks.get(winner).isEmpty()) {
+      score = score + decks.get(winner).removeLast() * i;
+      i++;
+    }
+    return score;
+  }
+
   public static void main(String[] args) throws IOException {
     CrabCombat obj = new CrabCombat();
     obj.getDecks();
-    System.out.println(obj.getCombatScore());
+    System.out.println("Puzzle 1 solution = " + obj.getCombatScore());
+    obj.getDecks();
+    System.out.println("Puzzle 2 solution = " + obj.getRecursiveCombatScore());
   }
 }
