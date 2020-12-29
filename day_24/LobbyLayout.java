ccb5e7fe8c9d317ca42e377be316392b9d446ad8
Akhil Sudhakaran <akhil.sudhakaran@microfocus.com>
2020-12-30 00:29:07 +0530

    Added solution for day 24 puzzle 2

--- day_24/LobbyLayout.java
+++ day_24/LobbyLayout.java
@@ -2,18 +2,19 @@
 package day_24;
 
 import java.io.*;
-import java.util.*;
 public class LobbyLayout {
-  static Map<String, Boolean> tileType;
+  static boolean[][] tileType;
+  // static Map<String, Boolean> tileType;
 
   void collectTileList() throws IOException {
     BufferedReader br = new BufferedReader(new FileReader("day_24/tiles.list"));
     String line;
-    tileType = new HashMap<String, Boolean>();
-    tileType.put("0,0", false);
+    tileType = new boolean[500][500];
+    int referenceX = 500/2;
+    int referenceY = 500/2;
     while((line = br.readLine()) != null) {
-      int x = 0;
-      int y = 0;
+      int x = referenceX;
+      int y = referenceY;
       for(int i = 0; i < line.length(); i++) {
         char coord = line.charAt(i);
         if(coord == 'n') {
@@ -47,47 +48,7 @@ public class LobbyLayout {
           x -= 2;
         }
       }
-      String key = x + "," + y;
-      if(tileType.containsKey(key)) {
-        tileType.put(key, !tileType.get(key));
-      }
-      else {
-        tileType.put(key, true);
-        x = x - 2;
-        key = x + "," + y;
-        if(!tileType.containsKey(key)) {
-          tileType.put(key, false);
-        }
-        x = x + 2;
-        key = x + "," + y;
-        if(!tileType.containsKey(key)) {
-          tileType.put(key, false);
-        }
-        x = x - 1;
-        y = y - 1;
-        key = x + "," + y;
-        if(!tileType.containsKey(key)) {
-          tileType.put(key, false);
-        }
-        x = x - 1;
-        y = y + 1;
-        key = x + "," + y;
-        if(!tileType.containsKey(key)) {
-          tileType.put(key, false);
-        }
-        x = x + 1;
-        y = y + 1;
-        key = x + "," + y;
-        if(!tileType.containsKey(key)) {
-          tileType.put(key, false);
-        }
-        x = x + 1;
-        y = y - 1;
-        key = x + "," + y;
-        if(!tileType.containsKey(key)) {
-          tileType.put(key, false);
-        }
-      }
+      tileType[x][y] = !tileType[x][y];
     }
     br.close();
     return;
@@ -95,157 +56,77 @@ public class LobbyLayout {
 
   int blackSideCount() {
     int count = 0;
-    for(boolean side: tileType.values()) {
-      if(side) {
-        count += 1;
+    for(int i = 0; i < tileType.length; i++) {
+      for(int j = 0; j < tileType[i].length; j++) {
+        if(tileType[i][j]) {
+          count += 1;
+        }
       }
+    }
+    return count;
+  }
+
+  int getBlackNeighbourCount(boolean[][] tileTypeCopy, int i, int j) {
+    int count  = 0;
+    int x = i - 2;
+    int y = j;
+    if(x >= 0 && tileTypeCopy[x][y]) {
+      count++;
+    }
+    x = i + 2;
+    if(x < tileTypeCopy.length && tileTypeCopy[x][y]) {
+      count++;
+    }
+    x = i - 1;
+    y = j - 1;
+    if(y >= 0 && x >= 0 && tileTypeCopy[x][y]) {
+      count++;
+    }
+    x = i - 1;
+    y = j + 1;
+    if(y < tileTypeCopy[i].length && x >= 0 && tileTypeCopy[x][y]) {
+      count++;
+    }
+    x = i + 1;
+    y = j + 1;
+    if(y < tileTypeCopy[i].length && x < tileTypeCopy.length && tileTypeCopy[x][y]) {
+      count++;
+    }
+    x = i + 1;
+    y = j - 1;
+    if(y >= 0 && x < tileTypeCopy.length && tileTypeCopy[x][y]) {
+      count++;
     }
     return count;
   }
 
   int blackSideCountAfterChanges(int iterations) {
-    for(int i = 0; i < iterations; i++) {
-      Map<String, Boolean> tileTypeCopy = new HashMap<String, Boolean>(tileType);
-      for(String key: tileTypeCopy.keySet()) {
-        int xc = Integer.parseInt(key.split(",")[0]);
-        int yc = Integer.parseInt(key.split(",")[1]);
-        if(tileTypeCopy.get(key)) {
-          int count = 0;
-          String keyCopy = key;
-          int x = xc - 2;
-          key = x + "," + yc;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc + 2;
-          key = x + "," + yc;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc - 1;
-          int y = yc - 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc - 1;
-          y = yc + 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc + 1;
-          y = yc + 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc + 1;
-          y = yc - 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-
-          if(count == 0 || count > 2) {
-            tileType.put(keyCopy, false);
-          }
+    for(int iter = 0; iter < iterations; iter++) {
+      boolean[][] tileTypeCopy = new boolean[500][500];
+      for(int i = 0; i < tileType.length; i++) {
+        for(int j = 0; j < tileType[i].length; j++) {
+          tileTypeCopy[i][j] = tileType[i][j];
         }
-        else {
-          int count = 0;
-          String keyCopy = key;
-          int x = xc - 2;
-          key = x + "," + yc;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
+      }
+      for(int i = 0; i < tileTypeCopy.length; i++) {
+        for(int j = 0; j < tileTypeCopy[i].length; j++) {
+          int count = getBlackNeighbourCount(tileTypeCopy, i, j);
+          if(tileTypeCopy[i][j] && (count == 0 || count > 2)) {
+            tileType[i][j] = false;
           }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc + 2;
-          key = x + "," + yc;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc - 1;
-          int y = yc - 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc - 1;
-          y = yc + 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc + 1;
-          y = yc + 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-          x = xc + 1;
-          y = yc - 1;
-          key = x + "," + y;
-          if(!tileTypeCopy.containsKey(key)) {
-            tileType.put(key, false);
-          }
-          else {
-            count += (tileTypeCopy.get(key)) ? 1 : 0;
-          }
-
-          if(count == 2) {
-            tileType.put(keyCopy, true);
+          else if(count == 2) {
+            tileType[i][j] = true;
           }
         }
       }
     }
-    int count = 0;
-    for(boolean side: tileType.values()) {
-      if(side) {
-        count += 1;
-      }
-    }
-    return count;
+    return blackSideCount();
   }
 
   public static void main(String[] args) throws IOException {
     LobbyLayout obj = new LobbyLayout();
     obj.collectTileList();
-    System.out.println(obj.blackSideCount());
-    System.out.println(tileType);
-    System.out.println(obj.blackSideCountAfterChanges(1));
-    System.out.println(tileType);
+    System.out.println("Puzzle 1 solution = " + obj.blackSideCount());
+    System.out.println("Puzzle 2 solution = " + obj.blackSideCountAfterChanges(100));
   }
 }
